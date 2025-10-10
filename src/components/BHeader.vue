75a389313c7afbdd67cdaee78bace288f49dcdaa
Bohan Zhang <bzha0129@student.monash.edu>
2025-10-10 19:17:22 +1100

    W9-9.3

--- src/components/BHeader.vue
+++ src/components/BHeader.vue
@@ -56,6 +56,9 @@ onMounted(() => {
         <li class="nav-item">
           <router-link to="/addbook" class="nav-link" active-class="active">Add Book</router-link>
         </li>
+        <li class="nav-item">
+          <router-link to="/GetBookCount" class="nav-link" active-class="active">Get Book Count</router-link>
+        </li>
         <li v-if="isAuthenticated" class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" @click="toggleDropdown" aria-expanded="false">
             Welcome, {{ username }}
