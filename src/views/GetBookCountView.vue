37eed7dd94a566d2d7a7e7d459cdca76163e0581
Bohan Zhang <bzha0129@student.monash.edu>
2025-10-10 20:12:22 +1100

    W9-9.4

--- src/views/GetBookCountView.vue
+++ src/views/GetBookCountView.vue
@@ -20,7 +20,7 @@ export default {
   methods: {
     async getBookCount() {
       try {
-        const response = await axios.get('https://[redacted]-week7-yiwei.cloudfunctions.net/countBooks');
+        const response = await axios.get('https://us-central1-week7-bohan.cloudfunctions.net/countBooks');
         this.count = response.data.count;
         this.error = null;
       } catch (error) {
